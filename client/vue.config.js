c95337c822a720d0d5ca8e84338b464ec55157d2
Nikita Marcius <nikitamarcius@users.noreply.github.com>
2020-07-15 00:13:55 +0300

    publicPath: '/'

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-    runtimeCompiler: true, 
-    // '/sovdex/' path need for gh-pages
-    publicPath: process.env.NODE_ENV == 'production' ? '/sovdex/' : '/',
-    pages: {
-        index: {
-            entry: 'src/main.js',
-            template: 'public/index.html',
-            filename: 'index.html',
-        }
-    },
-    configureWebpack: {
-        output: {
-          filename: '[name].js',
-          chunkFilename: '[name].js'
-        }
-      }
+  runtimeCompiler: true,
+  // '/sovdex/' path need for gh-pages
+  publicPath: '/', // process.env.NODE_ENV == 'production' ? '/sovdex/' : '/',
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+    }
+  },
+  configureWebpack: {
+    output: {
+      filename: '[name].js',
+      chunkFilename: '[name].js'
+    }
+  }
 }
